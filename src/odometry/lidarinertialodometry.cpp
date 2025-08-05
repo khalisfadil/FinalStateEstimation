e19dbe218ceff4cd5e5d01ce906d0f66f9869cc7
Khalis Fadil <khalis_muse@yahoo.com>
2025-08-05 13:00:18 +0200

    correcting debug line

--- src/odometry/lidarinertialodometry.cpp
+++ src/odometry/lidarinertialodometry.cpp
@@ -2232,7 +2232,7 @@ namespace  stateestimate{
                 }
 
 #ifdef DEBUG
-                    std::cout << "[ICP DEBUG] IMU Data: " << std::setprecision(4) << imu.timestamp << " " << imu.ang_vel.transpose() << " " << imu.lin_acc.transpose() << std::endl;
+                    std::cout << "[ICP DEBUG] IMU Data: " << std::setprecision(4) << imu_data.timestamp << " " << imu_data.ang_vel.transpose() << " " << imu_data.lin_acc.transpose() << std::endl;
 #endif
 
                 // Interpolate IMU biases between knots
